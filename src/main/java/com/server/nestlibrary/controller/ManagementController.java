036f0922c1ea3807075d7376c4844c74e2205d11
Parkseongchul97 <dol9991@naver.com>
2024-10-18 18:00:22 +0900

    구독한 채널 리스트

--- src/main/java/com/server/nestlibrary/controller/ManagementController.java
+++ src/main/java/com/server/nestlibrary/controller/ManagementController.java
@@ -1,5 +1,6 @@
 package com.server.nestlibrary.controller;
 
+import com.server.nestlibrary.model.dto.ChannelDTO;
 import com.server.nestlibrary.model.vo.Management;
 import com.server.nestlibrary.service.ManagementService;
 import lombok.extern.slf4j.Slf4j;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -37,5 +40,12 @@ public class ManagementController {
     public ResponseEntity check(@PathVariable  (name="channelCode") int channelCode) {
             return ResponseEntity.ok(managementService.check(channelCode));
     }
+    // 내가 구독중인 채널
+    @GetMapping("/private/subscribe/channel")
+    public ResponseEntity myChannel() {
+        List<ChannelDTO> list = managementService.mySubscribe();
+        return ResponseEntity.ok(list);
+    }
+
 
 }
--- src/main/java/com/server/nestlibrary/service/ChannelService.java
+++ src/main/java/com/server/nestlibrary/service/ChannelService.java
@@ -9,6 +9,7 @@ import com.server.nestlibrary.repo.ChannelTagDAO;
 import com.server.nestlibrary.repo.ManagementDAO;
 import com.server.nestlibrary.repo.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -24,6 +25,7 @@ public class ChannelService {
     private ChannelTagDAO tagDAO;
     @Autowired
     private ManagementDAO managementDAO;
+    @Lazy
     @Autowired
     private  ManagementService managementService;
     @Autowired
